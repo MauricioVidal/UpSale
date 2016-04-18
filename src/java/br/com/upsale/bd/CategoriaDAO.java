5954557cd72f9d51bea16c56518513fb094a06d1
a14020 <a14020@B208-P99288>
2016-04-18 10:08:43 -0300

    Implementando DAO Usuario, Estoque e Categoria.

---
+++ src/java/br/com/upsale/bd/CategoriaDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.upsale.bd;
+
+import br.com.upsale.model.Categoria;
+import java.util.List;
+
+/**
+ *
+ * @author a14020
+ */
+public class CategoriaDAO implements DAO<Categoria>{
+
+    @Override
+    public List<Categoria> getLista() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean atualizar(Categoria o) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean inserir(Categoria o) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean remover(Categoria o) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
