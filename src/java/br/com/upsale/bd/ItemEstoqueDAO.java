9abf20d0f532d59ed88a2da1b3542548e4338cb9
marcelo <marcelo@Dell>
2016-05-12 10:10:09 -0300

    Implementação das classes ItemEstoqueDAO.java e ItemVendaDAO.java

--- src/java/br/com/upsale/bd/ItemEstoqueDAO.java
+++ src/java/br/com/upsale/bd/ItemEstoqueDAO.java
@@ -5,7 +5,14 @@
  */
 package br.com.upsale.bd;
 
+import static br.com.upsale.bd.DAO.connectionFactory;
+import br.com.upsale.model.Categoria;
 import br.com.upsale.model.ItemEstoque;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,22 +23,65 @@ public class ItemEstoqueDAO implements DAO<ItemEstoque> {
 
     @Override
     public List<ItemEstoque> getLista() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ItemEstoque> lista = new ArrayList<>();
+        String sql = String.format(connectionFactory.getSQLSelect(), "itemEstoque");
+        Connection con = connectionFactory.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            ItemEstoque i = new ItemEstoque();
+            i.setId_estoque(rs.getLong("id_estoque"));
+            i.setId_produto(rs.getLong("id_produto"));
+            i.setQuantidade(rs.getInt("quantidade"));
+            lista.add(i);
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return lista;
     }
 
     @Override
     public boolean atualizar(ItemEstoque o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = String.format(connectionFactory.getSQLUpdate(), "itemEstoque",
+                "quantidade", "?", "id_estoque = ? and id_produto = ?");
+        Connection con = connectionFactory.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, o.getQuantidade());
+        ps.setLong(2, o.getId_estoque());
+        ps.setLong(3, o.getId_produto());
+        boolean rs = ps.executeUpdate() == 1;
+        ps.close();
+        con.close();
+        return rs;
     }
 
     @Override
     public boolean inserir(ItemEstoque o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = String.format(connectionFactory.getSQLInsert(), "itemEstoque",
+                "id_estoque, id_produto, quantidade", "?,?,?");
+        Connection con = connectionFactory.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setLong(1, o.getId_estoque());
+        ps.setLong(2, o.getId_produto());
+        ps.setInt(3, o.getQuantidade());
+        boolean rs = ps.executeUpdate() == 1;
+        ps.close();
+        con.close();
+        return rs;
     }
 
     @Override
     public boolean remover(ItemEstoque o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = String.format(connectionFactory.getSQLDelete(), "usuario", "id_estoque = ? and id_produto = ?");
+        Connection con = connectionFactory.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setLong(1, o.getId_estoque());
+        ps.setLong(2, o.getId_produto());
+        boolean rs = ps.executeUpdate() == 1;
+        ps.close();
+        con.close();
+        return rs;
     }
-    
+
 }
--- src/java/br/com/upsale/bd/ItemVendaDAO.java
+++ src/java/br/com/upsale/bd/ItemVendaDAO.java
@@ -5,33 +5,83 @@
  */
 package br.com.upsale.bd;
 
+import static br.com.upsale.bd.DAO.connectionFactory;
+import br.com.upsale.model.ItemEstoque;
 import br.com.upsale.model.ItemVenda;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
  * @author Mauricio R. Vidal
  */
-public class ItemVendaDAO implements DAO<ItemVenda>{
+public class ItemVendaDAO implements DAO<ItemVenda> {
 
     @Override
     public List<ItemVenda> getLista() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ItemVenda> lista = new ArrayList<>();
+        String sql = String.format(connectionFactory.getSQLSelect(), "itemEstoque");
+        Connection con = connectionFactory.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            ItemVenda i = new ItemVenda();
+            i.setId_produto(rs.getLong("id_produto"));
+            i.setId_venda(rs.getLong("id_venda"));
+            i.setQuantidade(rs.getInt("quantidade"));
+            lista.add(i);
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return lista;
     }
 
     @Override
     public boolean atualizar(ItemVenda o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = String.format(connectionFactory.getSQLUpdate(), "itemVenda",
+                "quantidade", "?", "id_produto = ? and id_venda = ?");
+        Connection con = connectionFactory.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, o.getQuantidade());
+        ps.setLong(2, o.getId_produto());
+        ps.setLong(3, o.getId_venda());
+        boolean rs = ps.executeUpdate() == 1;
+        ps.close();
+        con.close();
+        return rs;
     }
 
     @Override
     public boolean inserir(ItemVenda o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = String.format(connectionFactory.getSQLInsert(), "itemEstoque",
+                "id_produto, id_venda, quantidade", "?,?,?");
+        Connection con = connectionFactory.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setLong(1, o.getId_produto());
+        ps.setLong(2, o.getId_venda());
+        ps.setInt(3, o.getQuantidade());
+        boolean rs = ps.executeUpdate() == 1;
+        ps.close();
+        con.close();
+        return rs;
     }
 
     @Override
     public boolean remover(ItemVenda o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = String.format(connectionFactory.getSQLDelete(), "usuario", "id_produto = ? and id_venda = ?");
+        Connection con = connectionFactory.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setLong(1, o.getId_produto());
+        ps.setLong(2, o.getId_venda());
+        boolean rs = ps.executeUpdate() == 1;
+        ps.close();
+        con.close();
+        return rs;
     }
-    
+
 }
